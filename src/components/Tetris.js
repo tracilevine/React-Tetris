712c1b836de9643425b53e9724570d8d24a85aa3
tracilevine <televine@gmail.com>
2020-11-09 18:31:04 -0500

    created customs Hooks, beginning setting up functions in the tetris.js

--- src/components/Tetris.js
+++ src/components/Tetris.js
@@ -1,30 +1,74 @@
-import React from 'react'
-import {createStage} from '../gameHelpers';
-import { StyledTetrisWrapper, StyledTetris } from './styles/StyledTetris';
+import React, { useState } from "react";
+import {createStage } from '../gameHelpers';
 
+import { usePlayer } from "../Hooks/usePlayer";
+import { useStage } from "../Hooks/useStage";
 
-import Stage from './Stage'
-import Display from './Display'
-import StartButton from './StartButton'
+import { StyledTetrisWrapper, StyledTetris } from "./styles/StyledTetris";
 
-const Tetris = () =>{
+import Stage from "./Stage";
+import Display from "./Display";
+import StartButton from "./StartButton";
 
-    return (
-        <StyledTetrisWrapper>
-            <StyledTetris>
-            <Stage stage={createStage()}/> 
-            <aside>
-                <div>
-                <Display text="Score"/>
-                <Display text="Rows"/>
-                <Display text="Level"/>
-                </div>
-                <StartButton />
-                
-            </aside>
-            </StyledTetris>
-        </StyledTetrisWrapper>
-    )
+const Tetris = () => {
+  const [dropTime, setDropTime] = useState(null);
+  const [gameOver, setGameOver] = useState(false);
+
+  const [player] = usePlayer();
+  const [stage, setStage] = useStage(player);
+
+  console.log("re-render");
+
+const movePlayer = dir => { 
+    updatePlayerPos({ x: dir, y: 0 })
 }
+
+const startGame = ()=>{
+    setStage(createStage());
+    resetPlayer();
+}
+
+const drop =()=>{
+    updatePlayerPos({ x: 0, y: 1, collided: false})
+}
+
+const dropPlayer =()=>{
+    drop();
+}
+
+const move = ({keyCode})=>{
+    if (!gameOver){
+        if (keyCode=== 37){
+            movePlayer(-1);
+        }
+        else if (keyCode === 39){
+            movePlayer(1);
+        }
+        else if (keyCode === 40){
+           dropPlayer(); 
+        }
+    }
+}
+
+  return (
+    <StyledTetrisWrapper role="button" tabIndex="0" onKeyDown={e => move(e)} onKeyUp={keyUp}>
+      <StyledTetris>
+        <Stage stage={stage} />
+        <aside>
+          {gameOver ? (
+            <Display gameOver={gameOver} text="Game Over" />
+          ) : (
+            <div>
+              <Display text={`Score: ${score}`} />
+              <Display text={`Rows: ${rows}`} />
+              <Display text={`Levels: ${level}`} />
+            </div>
+          )}
+          <StartButton onClick={startGame}/>
+        </aside>
+      </StyledTetris>
+    </StyledTetrisWrapper>
+  );
+};
 
 export default Tetris;
